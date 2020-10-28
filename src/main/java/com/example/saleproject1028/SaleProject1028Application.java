bae96720fcbb1417da1e174a24789a9993b01412
zbc <1151624001@qq.com>
2020-10-28 21:37:34 +0800

    Initial commit

---
+++ src/main/java/com/example/saleproject1028/SaleProject1028Application.java
@@ -0,0 +1,13 @@
+package com.example.saleproject1028;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleProject1028Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleProject1028Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saleproject1028/SaleProject1028ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saleproject1028;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleProject1028ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
