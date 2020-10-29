f715484361dc2767610335e14ecede42f00f17a1
zhangbochao <1151624001@qq.com>
2020-10-29 10:52:48 +0800

    dada

--- src/main/java/com/example/saleproject1028/controller/SaleController.java
+++ src/main/java/com/example/saleproject1028/controller/SaleController.java
@@ -1,12 +1,30 @@
 package com.example.saleproject1028.controller;
 
+import com.example.saleproject1028.pojo.Sale;
+import com.example.saleproject1028.service.ProductService;
+import com.example.saleproject1028.service.SaleService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class SaleController {
+    private SaleService saleService;
+    private ProductService productService;
     @RequestMapping("/")
     public String show(){
         return "show";
+    }
+    @RequestMapping("zzx")
+    @ResponseBody
+    public PageInfo findAllDUser(Integer sdd,Integer pageNo){
+        PageHelper.offsetPage((pageNo-1)*4,4);
+        List<Sale> duserList=saleService.findAll(sdd);
+        PageInfo<Sale> pageInfo=new PageInfo<Sale>(duserList);
+        return pageInfo;
     }
 }
